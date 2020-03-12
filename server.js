831052c9be453e93b6bb13380191971430d4dcb6
sajidhassann <sajidhassan1997@gmail.com>
2020-03-12 10:21:27 +0500

    MongoDB setup

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-//Routes
+//Connect DB
+connectDB();
 
+app.get('/', (req, res) => {
+  res.json({ msg: 'Welcome to the Contactkeeper API...' });
+});
+
+//Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
