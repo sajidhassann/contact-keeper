20408617d65a3bc2be59790e427ed3d368eeb7b6
sajidhassann <sajidhassan1997@gmail.com>
2020-04-29 16:44:58 +0500

    error fixing

--- routes/users.js
+++ routes/users.js
@@ -33,7 +33,7 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (user) {
-        return res.status(400).json({ msg: 'User alredy exist' });
+        return res.status(400).json({ msg: 'User already exists' });
       }
 
       user = new User({
